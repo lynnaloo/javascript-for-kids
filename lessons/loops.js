deea4e2869e407be936650183c998f57cdf9edff
Linda Nichols <lynnaloo@gmail.com>
2014-08-22 05:28:47 +0000

    Adding lessons to lead up to game

---
+++ lessons/loops.js
@@ -0,0 +1,3 @@
+// for loop
+
+// while loop
---
+++ lessons/outputs-and-variables.js
@@ -0,0 +1,20 @@
+// Create a popup with the phrase
+alert("hello world");
+
+// Display the result of a math problem
+alert(2 + 2);
+
+// Display the result of adding two strings
+alert("hello" + " " + "world");
+
+// Make a name for some other value
+var hello = "hello world";
+// display the value saved as hello
+alert(hello);
+
+// Display a popup that asks for an answer
+prompt("hello world?");
+// Display the popup again and save the answer as a variable
+var answer = prompt("hello world?");
+// Display the answer
+alert(answer);
