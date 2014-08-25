9ea484ea8989f9d3de8bd637d9fed86d51df0b07
lynnaloo <lynnaloo@gmail.com>
2014-08-24 23:59:50 -0400

    shortened variable names

--- word-guess.js
+++ word-guess.js
@@ -4,44 +4,43 @@
 // wrong three times.
 
 // Create a variable to store the number of bad guesses
-var bad_guesses = 0;
+var strikes = 0;
 // Create another array to store good guesses
-var good_guesses = [];
+var letters = [];
 
 // Prompt Player 1 to enter a word to guess and store
 // as a variable.
-// TODO: put this in a loop until a valid response is received.
-var secret_word = prompt("Player 1: Enter your secret word.");
+var word = prompt("Player 1: Enter your secret word.");
 
 // Fill this array with placeholders for guessing
-for (i = 0; i < secret_word.length; i++) {
-  good_guesses.push("_");
+for (i = 0; i < word.length; i++) {
+  letters.push("_");
 }
 
 // Start a loop that continues as long as the person has
 // not guessed wrong three times, or all of the letters have
 // been guessed.
-while (bad_guesses !== 3 && good_guesses.indexOf("_") !== -1) {
+while (strikes !== 3 && letters.indexOf("_") !== -1) {
 
   // Prompt Player 2 to guess a letter and store as
   // a variable.
   // TODO: put this in a loop until a valid response is received.
-  var guess = prompt(good_guesses.join(" ") + "\n\n" + "Player 2: Guess a letter.");
+  var guess = prompt(letters.join(" ") + "\n\n" + "Player 2: Guess a letter.");
 
   // If the letter does not exist in the word,
   // add it to the bad guesses.
-  if (secret_word.indexOf(guess) === -1) {
-    bad_guesses++;
+  if (word.indexOf(guess) === -1) {
+    strikes++;
     alert("Bad guess!");
 
   // If the letter exists in the word, we need to
   // add it to the good guesses array
   } else {
-    for (i = 0; i < secret_word.length; i++) {
+    for (i = 0; i < word.length; i++) {
       // Each time the guess letter is found, we
       // add it as a good guess in the same spot
-        if (secret_word[i] === guess) {
-          good_guesses[i] = guess;
+        if (word[i] === guess) {
+          letters[i] = guess;
         }
     }
   }
@@ -50,9 +49,9 @@ while (bad_guesses !== 3 && good_guesses.indexOf("_") !== -1) {
 // Once the player has exited the loop, congratulate
 // them on a win, or tell them they have lost and show
 // the secret word.
-if (bad_guesses === 3) {
+if (strikes === 3) {
   alert("Sorry, please play again!");
 } else {
   alert("Congratulations on your win!");
 }
-alert("The secret word was " + secret_word);
+alert("The secret word was " + word);
